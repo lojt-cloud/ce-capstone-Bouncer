1641bd32a1f87d2326dd52eba6c175b1ddc01862
lojt-cloud <264168469+lojt-cloud@users.noreply.github.com>
2026-08-29 20:00:48 +0200

    Foundation: security module (alb/app/db/cache SGs, least-privilege)

--- terraform/environments/dev/foundation/outputs.tf
+++ terraform/environments/dev/foundation/outputs.tf
@@ -1,3 +1,4 @@
+#VPC Outputs
 output "vpc_id" {
   value = module.networking.vpc_id
 }
@@ -6,6 +7,8 @@ output "vpc_cidr" {
   value = module.networking.vpc_cidr
 }
 
+
+#Subnet Outputs
 output "public_subnet_ids" {
   value = module.networking.public_subnet_ids
 }
@@ -13,3 +16,22 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.networking.private_subnet_ids
 }
+
+#SG IDs
+output "alb_security_group_id" {
+  value = module.security.alb_security_group_id
+}
+
+output "app_security_group_id" {
+  value = module.security.app_security_group_id
+}
+
+output "db_security_group_id" {
+  value = module.security.db_security_group_id
+}
+
+output "cache_security_group_id" {
+  value = module.security.cache_security_group_id
+}
+
+#
