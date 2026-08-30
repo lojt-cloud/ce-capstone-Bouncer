545e3a923d0722f36d7e0cedd678d2d420f5360a
lojt-cloud <264168469+lojt-cloud@users.noreply.github.com>
2026-08-30 17:14:07 +0200

    terraform fmt

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -151,11 +151,11 @@ resource "aws_iam_role_policy" "flow_logs_publish" {
 }
 
 resource "aws_flow_log" "vpc" {
-  vpc_id                = aws_vpc.this.id
-  traffic_type           = "ALL"
-  log_destination_type   = "cloud-watch-logs"
-  log_destination        = aws_cloudwatch_log_group.vpc_flow_logs.arn
-  iam_role_arn            = aws_iam_role.flow_logs.arn
+  vpc_id               = aws_vpc.this.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn         = aws_iam_role.flow_logs.arn
 
   tags = {
     Name = "${var.name_prefix}-vpc-flow-log"
